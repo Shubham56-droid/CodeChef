5b7090214a7f59e57e74bb17eeccaa628cb76db1
Shubham Bawankar <71594948+Shubham56-droid@users.noreply.github.com>
2022-02-20 22:45:39 +0530

    Create sumOfSubarray.cpp

---
+++ sumOfSubarray.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int size;
+    cin>>size;
+    int arr[size];
+    for (int i = 0; i < size; i++)
+    {
+        cin>>arr[i];
+    }
+
+    int sum = 0;
+    for (int i = 0; i < size; i++)
+    {
+        sum = 0;
+        for (int j = i; j < size; j++)
+        {
+           sum = sum + arr[j]; 
+           cout<<sum<<"\n";
+        }  
+    }
+    
+    
+    return 0;
+}
